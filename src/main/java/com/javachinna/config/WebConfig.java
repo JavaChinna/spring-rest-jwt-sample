194911e835e821c82ce4ea02dbb403d405e9bca0
Chinna <inbox4chinna@gmail.com>
2020-02-12 12:10:16 +0530

    Added Spring Boot Actuator 2.X Support

--- src/main/java/com/javachinna/config/WebConfig.java
+++ src/main/java/com/javachinna/config/WebConfig.java
@@ -2,8 +2,11 @@ package com.javachinna.config;
 
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,6 +15,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @Configuration
 @ComponentScan("com.javachinna")
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+@PropertySource(value = { "classpath:application.properties" })
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
